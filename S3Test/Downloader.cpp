82fef67606bc141fda9a37adbb88678c25362777
kreuzerkrieg <kreuzerkrieg@gmail.com>
2019-07-21 08:22:10 +0300

    S3File handling

--- S3Test/Downloader.cpp
+++ S3Test/Downloader.cpp
@@ -1,68 +1,85 @@
 #include "Downloader.h"
+#include "Fs.h"
+#include "S3File.h"
 #include <aws/s3/S3Client.h>
 #include <aws/s3/model/GetObjectRequest.h>
 #include <aws/s3/model/HeadObjectRequest.h>
 #include <deque>
 #include <fstream>
 
-void DownloadFile(const std::string& bucket, const fs::path& file_name, const fs::path& target_file)
+void FileDownloader::Download(const fs::path& fully_qualified_name, const fs::path& target_file)
 {
-    Aws::S3::S3Client s3_client;
+    std::lock_guard lck(download_in_progress);
 
-    Aws::S3::Model::HeadObjectRequest head_object_request;
-    head_object_request.SetBucket(bucket.c_str());
-    head_object_request.SetKey(file_name.c_str());
-    auto outcome = s3_client.HeadObject(head_object_request);
-    size_t file_size = outcome.GetResult().GetContentLength();
+    downloading_thread = std::thread([this, &fully_qualified_name]() {
+        S3File s3file(fully_qualified_name);
+        size_t file_size = s3file.size();
 
-    // Get the object
-    std::ofstream file(target_file, std::ios::binary | std::ios::trunc);
-    std::deque<std::future<Aws::S3::Model::GetObjectResult>> futures;
-    auto begin = std::chrono::high_resolution_clock::now();
-
-    std::promise<Aws::S3::Model::GetObjectResult> promise;
-    auto future = promise.get_future();
-    constexpr size_t part_size = 20 * 1024 * 1024;
-    constexpr size_t threads_num = 64;
-    size_t part_count = 0;
-    while((part_count * part_size) <= file_size)
-    {
-        for(auto cycle = 0; cycle < threads_num; ++cycle)
+        constexpr size_t part_size = 20 * 1024 * 1024;
+        constexpr size_t parts_num = 64;
+        size_t part_count = 0;
+        while((part_count * part_size) <= file_size || !abort)
         {
             if((part_count * part_size) >= file_size)
+            {
+                has_work_to_do = false;
                 break;
-            futures.emplace_back(std::async(std::launch::async, [&, part{part_count++}]() {
-                Aws::S3::S3Client client;
-                Aws::S3::Model::GetObjectRequest object_request;
-                object_request.SetBucket(bucket.c_str());
-                object_request.SetKey(file_name.c_str());
-                auto end_range = std::min((part + 1) * part_size - 1, file_size);
-                std::string range = std::string("bytes=") + std::to_string(part * part_size) + "-" + std::to_string(end_range);
-                std::cout << "Downloading part " << part << " range: from " << part * part_size << " to " << end_range << std::endl;
-                object_request.SetRange(range.c_str());
-                auto get_object_outcome = s3_client.GetObject(object_request);
-                if(get_object_outcome.IsSuccess())
-                {
-                    return get_object_outcome.GetResultWithOwnership();
-                }
-                else
-                {
-                    auto error = get_object_outcome.GetError();
-                    throw std::runtime_error((Aws::String("ERROR: ") + error.GetExceptionName() + ": " + error.GetMessage()).c_str());
-                }
-            }));
+            }
+
+            auto part = part_count++;
+            std::string bucket_name;
+            std::string object_name;
+            S3fqn2parts(fully_qualified_name, bucket_name, object_name);
+            Aws::S3::Model::GetObjectRequest object_request;
+            object_request.SetBucket(bucket_name.c_str());
+            object_request.SetKey(object_name.c_str());
+            auto end_range = std::min((part + 1) * part_size - 1, file_size);
+            std::string range = std::string("bytes=") + std::to_string(part * part_size) + "-" + std::to_string(end_range);
+            std::cout << "Downloading part " << part << " range: from " << part * part_size << " to " << end_range << std::endl;
+            object_request.SetRange(range.c_str());
+
+            {
+                std::unique_lock<std::mutex> lck(download_mtx);
+                cv.wait(lck, [this] { return part_completion.size() < parts_num; });
+                part_completion.emplace_back(s3_client.GetObjectCallable(object_request));
+            }
+
+            cv.notify_all();
         }
-        while(!futures.empty())
+    });
+
+    std::ofstream file(target_file, std::ios::binary | std::ios::trunc);
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    while(has_work_to_do || !abort)
+    {
+        Aws::S3::Model::GetObjectResult part;
         {
-            file << futures.front().get().GetBody().rdbuf();
-            futures.pop_front();
+            std::unique_lock<std::mutex> lck(download_mtx);
+            cv.wait(lck, [this] { return !part_completion.empty(); });
+
+            auto result = part_completion.front().get();
+            if(!result.IsSuccess())
+            {
+                abort = true;
+                auto error = result.GetError();
+                throw std::runtime_error((Aws::String("ERROR: ") + error.GetExceptionName() + ": " + error.GetMessage()).c_str());
+            }
+            part = result.GetResultWithOwnership();
+            part_completion.pop_front();
         }
+        file << part.GetBody().rdbuf();
+        cv.notify_all();
     }
     auto end = std::chrono::high_resolution_clock::now();
+
+    S3File s3file(fully_qualified_name);
+    size_t file_size = s3file.size();
     std::cout << "File downloaded and saved in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms."
               << std::endl
               << "File size: " << file_size << std::endl
               << "Speed: "
               << double(file_size) / std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() * 1000 / 1024 / 1024
               << "MiB/s" << std::endl;
+    downloading_thread.join();
 }
--- S3Test/Downloader.h
+++ S3Test/Downloader.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include <aws/s3/S3Client.h>
+#include <deque>
 #include <experimental/filesystem>
+
 namespace fs = std::experimental::filesystem;
 
-void DownloadFile(const std::string& bucket, const fs::path& file_name, const fs::path& target_file);
+class FileDownloader
+{
+public:
+    void Download(const fs::path& fully_qualified_name, const fs::path& target_file);
+
+private:
+    Aws::S3::S3Client s3_client;
+    std::deque<Aws::S3::Model::GetObjectOutcomeCallable> part_completion;
+    std::thread downloading_thread;
+    std::condition_variable cv;
+    std::mutex download_mtx;
+    std::mutex download_in_progress;
+    bool has_work_to_do = true;
+    bool abort = false;
+};
